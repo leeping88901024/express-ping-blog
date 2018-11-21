93def97903800266f1003047c5748e7707329a97
lee <leeping88901024@gmail.com>
2018-11-21 16:42:54 +0800

    Update header.js

--- routes/header.js
+++ routes/header.js
@@ -17,7 +17,9 @@ router.get('/header', (req,res) => {
                 return;
             }
             var { items } = docs[0];
-            res.send({items: items});
+            // 这是否可用 查询操作符优化
+            homeItems = items.filter( x => x.fields.purpose === "Home")
+            res.send({items: homeItems});
         });
         client.close();
     });
